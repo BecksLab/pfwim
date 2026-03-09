3d98a53738bf5e0f999e983242df5663b9035a0a
Tanya Strydom <tanya.strydom@icloud.com>
2026-03-09 12:14:02 +0000

    📌 dependency import

--- R/pfim.R-package.R
+++ R/pfim.R-package.R
@@ -5,6 +5,7 @@
 #' @importFrom dplyr any_vars
 #' @importFrom dplyr bind_rows
 #' @importFrom dplyr filter_all
+#' @importFrom dplyr sample_n
 #' @importFrom stats na.omit
 #' @importFrom stats setNames
 #' @importFrom tidyr %>%
