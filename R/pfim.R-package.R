db3501a062bb433cb347e80d7499d2ab5dc3cedd
Tanya Strydom <tanya.strydom@icloud.com>
2026-03-09 12:12:09 +0000

    📌 dependency management

---
+++ R/pfim.R-package.R
@@ -0,0 +1,13 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom dplyr any_vars
+#' @importFrom dplyr bind_rows
+#' @importFrom dplyr filter_all
+#' @importFrom stats na.omit
+#' @importFrom stats setNames
+#' @importFrom tidyr %>%
+#' @importFrom tidyselect all_of
+## usethis namespace: end
+NULL
